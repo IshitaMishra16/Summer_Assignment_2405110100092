177a18666d048de22e556f24190e34385792a764
Ishita Mishra <mishraishita2005@gmail.com>
2026-06-02 23:54:35 +0530

    Add files via upload

---
+++ containDuplicate.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+class containDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
